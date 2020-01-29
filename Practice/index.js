acb6be41ce20f2b93b814774bf30b54155e092dd
Kahlua <ggamini7@gmail.com>
2020-01-29 21:05:11 +0900

    update index.js

--- Practice/index.js
+++ Practice/index.js
@@ -1,90 +1,111 @@
 const nicoInfo = {
-  name: "Nico",
-  age: 33,
-  gender: "Male",
-  isHandsome: true,
-  favMovies: ["Along the gods", "LOTR", "Oldboy"],
-  favFood: [
-    {
-      name: "Kimchi",
-      fatty: false
-    },
-    {
-      name: "Cheese burger",
-      fatty: true
-    }
-  ]
-}
-//console.log(console)
-//console.log(nicoInfo, console)
-
-function sayHello(name, age){
-  return `Hello ${name} you are ${age} years old`;
-}
-
-const greetNicolas = sayHello("Nicolas", 15);
-
-//console.log(greetNicolas)
-
-const calculator = {
-  plus: function(){
-    const numberOfArgs = arguments.length;
-    let result = arguments[0];
-    if(numberOfArgs === 0){
-      return (result = 0);
-    }else{
-      for(let i = 1; i < arguments.length; i++){
-        result += arguments[i];
+    name: "Nico",
+    age: 33,
+    gender: "Male",
+    isHandsome: true,
+    favMovies: ["Along the gods", "LOTR", "Oldboy"],
+    favFood: [
+      {
+        name: "Kimchi",
+        fatty: false
+      },
+      {
+        name: "Cheese burger",
+        fatty: true
       }
-      return result;
-    }
-  },
-  minus:function(){
-    const numberOfArgs = arguments.length;
-    let result = arguments[0];
-    if(numberOfArgs === 0){
-      return (result = 0);
-    }else{
-      for(let i = 1; i < arguments.length; i++){
-        result -= arguments[i];
-      }
-      return result;
-    }
-  },
-  multiply:function(){
-    const numberOfArgs = arguments.length;
-    let result = arguments[0];
-    if(numberOfArgs === 0){
-      return (result = 0);
-    }else{
-      for(let i = 1; i < arguments.length; i++){
-        result *= arguments[i];
-      }
-      return result;
-    }
-  },
-  divide:function(){
-    const numberOfArgs = arguments.length;
-    let result = arguments[0];
-    if(numberOfArgs === 0){
-      return (result = 0);
-    }else{
-      for(let i = 1; i < arguments.lenth; i++){
-        result /= arguments[i];
-      }
-      return result;
-    }
-  },
-  power:function(a, b){
-    a ** b;
+    ]
   }
-}
+  //console.log(console)
+  //console.log(nicoInfo, console)
+  
+  function sayHello(name, age){
+    return `Hello ${name} you are ${age} years old`;
+  }
+  
+  const greetNicolas = sayHello("Nicolas", 15);
+  
+  //console.log(greetNicolas)
+  
+  const calculator = {
+    plus: function(){
+      const numberOfArgs = arguments.length;
+      let result = arguments[0];
+      if(numberOfArgs === 0){
+        return (result = 0);
+      }else{
+        for(let i = 1; i < arguments.length; i++){
+          result += arguments[i];
+        }
+        return result;
+      }
+    },
+    minus:function(){
+      const numberOfArgs = arguments.length;
+      let result = arguments[0];
+      if(numberOfArgs === 0){
+        return (result = 0);
+      }else{
+        for(let i = 1; i < arguments.length; i++){
+          result -= arguments[i];
+        }
+        return result;
+      }
+    },
+    multiply:function(){
+      const numberOfArgs = arguments.length;
+      let result = arguments[0];
+      if(numberOfArgs === 0){
+        return (result = 0);
+      }else{
+        for(let i = 1; i < arguments.length; i++){
+          result *= arguments[i];
+        }
+        return result;
+      }
+    },
+    divide:function(){
+      const numberOfArgs = arguments.length;
+      let result = arguments[0];
+      if(numberOfArgs === 0){
+        return (result = 0);
+      }else{
+        for(let i = 1; i < arguments.lenth; i++){
+          result /= arguments[i];
+        }
+        return result;
+      }
+    },
+    power:function(a, b){
+      a ** b;
+    }
+  }
+  
+  const plus = calculator.plus(5, 6);
+  //console.log(plus)
+  
+  //console.log(document);
+  
+  //const title = document.getElementById("title");
+  const title = document.querySelector("#title");    // id로 선택해보겠다, #으로 (css 선택자랑 비슷)
+  title.innerHTML = "Hi! From JS";
+  title.style.color = "red";
+  //console.dir(document)
+  document.title = "I own you now";
 
-const plus = calculator.plus(5, 6);
-//console.log(plus)
+  /*
+  function handleResize(){
+      console.log("I have been resized")
+  }
+  */
 
-//console.log(document);
+  function handleResize(event){
+      console.log(event);
+  }
 
-const title = document.getElementById("title");
+  window.addEventListener("resize", handleResize);     // 이벤트를 받기를 기다림.
 
-console.log(title)
+  function handleClick(){
+      title.style.color = "blue";
+  }
+
+  title.addEventListener("click", handleClick);
