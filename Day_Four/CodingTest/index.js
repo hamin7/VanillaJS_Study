ffae1eee5435dc60cd1769746ee6741d0e6c70dc
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-01-31 23:53:13 +0900

    Update index.js

--- Day_Four/CodingTest/index.js
+++ Day_Four/CodingTest/index.js
@@ -2,17 +2,16 @@
 import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
-const h2 = document.querySelector("h2");
 const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
 
 const superEventHandler = {
   handleResize: function() {
     if (window.innerWidth <= 400) {
-      document.bgColor = "blue";
+      document.bgColor = colors[1];
     } else if (window.innerWidth >= 400 && window.innerWidth <= 800) {
-      document.bgColor = "purple";
+      document.bgColor = colors[2];
     } else if (window.innerWidth >= 800) {
-      document.bgColor = "yello";
+      document.bgColor = colors[3];
     }
   }
 };
