4a0e90bc29d17ff14e0011d6c1a1ffb5b816ced8
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-01 01:17:43 +0900

    Update index.js

--- Day_Five/src/index.js
+++ Day_Five/src/index.js
@@ -1,25 +1,39 @@
 import "./styles.css";
 
 // You're gonna need this
-const NINE_HOURS_MILLISECONDS = 32400000;
+//const NINE_HOURS_MILLISECONDS = 32400000;
 
 const d_day = document.querySelector("d_day");
+//const current = document.querySelector("current");
 
 function getTime() {
   // Don't delete this.
   const now = new Date();
+  /*
+  const days = now.getDay();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const seconds = now.getSeconds();
+
+  current.innerText = `${days}d${hours < 10 ? `0${hours}` : hours}h${
+    minutes < 10 ? `0${minutes}` : minutes
+  }m${seconds < 10 ? `0${seconds}` : seconds}s`;
+  */
   const xmasDay = new Date("2020-12-24:00:00:00+0900");
   const gap = xmasDay.getTime() - now.getTime();
 
-  let day = Math.floor(gap / (1000 * 60 * 60 * 24));
-  let hour = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minuate = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
-  let second = Math.floor((gap % (1000 * 60)) / 1000);
+  const days = Math.floor(gap / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((gap % (1000 * 60)) / 1000);
 
-  d_day.innerText = day + "d" + hour + "h" + minuate + "m" + second + "s";
+  d_day.innerText = `${days}d${hours < 10 ? `0${hours}` : hours}h${
+    minutes < 10 ? `0${minutes}` : minutes
+  }m${seconds < 10 ? `0${seconds}` : seconds}s`;
 }
 
 function init() {
+  getTime();
   setInterval(getTime, 1000);
 }
 init();
