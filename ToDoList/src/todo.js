5d53296d46fa72cf644acea21ad4a0489408cac3
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-03 01:34:59 +0900

    Add files via upload

---
+++ ToDoList/src/todo.js
@@ -0,0 +1,37 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos"; // ToDos LocalStorage
+
+function paintToDo(text) {
+  //console.log(text);
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // default가 뭔데 그걸 방지한다는 거임?
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
