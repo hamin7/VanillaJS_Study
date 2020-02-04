208c5035693b5e7bbcfa61eec16a7a5b2a953197
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-04 20:55:52 +0900

    삭제기능 추가

--- Day_Nine/src/todo.js
+++ Day_Nine/src/todo.js
@@ -4,7 +4,26 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos"; // ToDos LocalStorage
 
-const toDos = [];
+// 이 array는 object가 있고 그 object는 숫자로 된 id가 있다.
+let toDos = [];
+
+// ToDo를 삭제하는 함수.
+function deleteToDo(event) {
+  // console.dir(event.target.parentNode); // 어떤 버튼이 클릭되었는지 알기 위해 event.target.
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);   // target의 parentNode인 li를 제거.
+
+  // 마치 forEach에서 function을 실행하는것 같이 각각의 item과 같이 실행 될 것.
+  // filter는 array 안에 있는 모든 toDos를 통해 filterFn 함수를 실행.
+  const cleanToDos = toDos.filter(function(toDo){
+    // toDo의 id는 숫자이고, li의 id는 string.
+    // 모든 toDos가 li의 id와 같지 않을 때.
+    return toDo.id !== parseInt(li.id);
+  });   // cleanToDos와 filter가 하는 것은 filterFn이 체크가 된 아이템들의 array를 주는 것.
+  toDos = cleanToDos;    // toDos는 이전 것이고, cleanToDos는 새로운 것.
+  saveToDos();    // toDos를 저장.
+}
 
 // toDos를 가져와서 localStorage에 저장하는 함수.
 function saveToDos() {
@@ -19,6 +38,7 @@ function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo); // 클릭하면 deleteToDo 함수 실행.
   const span = document.createElement("span");
   const newId = toDos.length + 1; // toDos 배열의 길이 + 1. 왜냐하면 id는 1부터 시작해야 하기 때문.
   span.innerText = text;
@@ -43,8 +63,19 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const loadedToDos = localStorage.getItem(TODOS_LS);
+  const loadedToDos = localStorage.getItem(TODOS_LS); // toDos 가져오기.
   if (loadedToDos !== null) {
+    // console.log(loadedToDos);
+    // 참고로 parse는 데이터를 전달할 때, 자바스크립트가 그걸 다룰 수 있도록 object(JSON)로 바꿔주는 기능.
+    const parsedToDos = JSON.parse(loadedToDos);
+    console.log(parsedToDos);
+    // forEach는 기본적으로 함수를 실행하는데 array에 담겨있는 것들 각각에 한번씩 함수를 실행시켜 주는 것.
+    // forEach안에 있는 함수를 parsedToDos에 있는 것들 각각에 대해 실행.
+    // forEach는 array를 위한 function.
+    parsedToDos.forEach(function(toDo) {
+      // console.log(toDo.text);
+      paintToDo(toDo.text); // 각각의 toDo.text에 대하여 paintToDo 실행.
+    });
   }
 }
 
