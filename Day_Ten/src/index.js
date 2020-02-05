fb4b1b38378b17c3702dfa35fd7dc32cebe67fee
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-05 14:33:26 +0900

    Add files via upload

---
+++ Day_Ten/src/index.js
@@ -0,0 +1,6 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const form = document.querySelector(".js-selectNumber");
+let input = form.input.querySelector("input");
