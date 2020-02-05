c7690f135d35c8b8ace7533b808a5a33382e76df
hamin7 <33690039+hamin7@users.noreply.github.com>
2020-02-06 02:32:17 +0900

    Update index.js

--- Day_Ten/src/index.js
+++ Day_Ten/src/index.js
@@ -3,7 +3,7 @@
 // <⚠️ /DONT DELETE THIS ⚠️>
 
 const form = document.getElementById("js-selectNumber");
-const input = form.querySelector("input");
+const button = document.getElementById("button");
 const choose = document.querySelector(".js-choose");
 const win = document.querySelector(".js-win");
 const range = document.querySelector(".js-range");
@@ -12,71 +12,65 @@ const rangeBar = document.querySelector(".slider_range");
 const SELECTED_NB = "selectedNumber";
 const MACHINECHOICE = "machineChoice";
 
-function loadMachineChoice(){
-    const loadedMachineChoice = localStorage.getItem(MACHINECHOICE);
-    if(loadedMachineChoice !== null){
-        const paredMachineChoice = JSON.parse(loadedMachineChoice);
-        console.log(parsedMachineChoice)
-    }
-    //return loadedMachineChoice;
+function paintChoose(user, machine) {
+  form.classList.remove(SELECTED_NB); // 이거 무슨 기능?
+  choose.classList.add(SELECTED_NB);
+  //console.log(machine);
+  const userChoice = parseInt(user);
+  const machineChoice = parseInt(machine);
+  choose.innerText = `You choose: ${userChoice}, the machine choose: ${machineChoice}`;
+  //console.log(userChoice, machineChoice);
+  if (userChoice >= machineChoice) {
+    win.innerText = `You Win`;
+  } else if (userChoice === machineChoice) {
+    win.innerText = `Draw`;
+  } else if (userChoice <= machineChoice) {
+    win.innerText = `You lost!`;
+  }
 }
 
-function saveMachineChoice(machineChoice){
-    localStorage.setItem(MACHINECHOICE, machineChoice);
+function loadMachineChoice() {
+  const loadedMachineChoice = localStorage.getItem(MACHINECHOICE);
+  if (loadedMachineChoice !== null) {
+    //console.log(loadedMachineChoice);
+    return loadedMachineChoice;
+  }
+  //return loadedMachineChoice;
 }
 
-/*
-function showSliderValue(sVal){
-    console.log(sVal)
-    //range.innerText = `Generate a Number between 0 and ${this.sVal}`
-}
-*/
-
-function paintChoose(user, machine){
-    form.classList.remove(SELECTED_NB);     // 이거 무슨 기능?
-    choose.classList.add(SELECTED_NB);
-    choose.innerText = `You choose: ${user}, the machine choose: ${machine}`
-    if(user > machine){
-        win.innerText = `You Win`
-    } else if(user === machine) {
-        win.innerText = `Draw`
-    } else if(user < machine) {
-        win.innerText = `You Loose`
-    } else {
-        win.innerText = `Fuck`
-    }
+function makeRandom(max) {
+  const min = 0;
+  //console.log(max);
+  const RandVal = Math.floor(Math.random() * (max - min + 1)) + min;
+  //console.log(RandVal);
+  return RandVal;
 }
 
-function loadChoose(){
-    const selectedNumber = localStorage.getItem(SELECTED_NB);
-    const computerChoice = makeRandom();
-    if(selectedNumber === null){
-    } else {
-        paintChoose(selectedNumber, computerChoice);
-    }
+function machineChoice(rangeMax) {
+  const max = rangeMax;
+  const machineChoice = makeRandom(max);
+  localStorage.setItem(MACHINECHOICE, machineChoice);
 }
 
-function handleFormSubmit(event){
-    const computerChoice = makeRandom();
-    const selectedNumber = input.value;
-    //console.log(selectedNumber);
-    paintChoose(selectedNumber, computerChoice);
+function handleFormSubmit(event) {
+  machineChoice(rangeBar.value);
+  const computerChoice = loadMachineChoice();
+  const selectedNumber = form.value;
+  //console.log(selectedNumber);
+  //console.log(computerChoice);
+  paintChoose(selectedNumber, computerChoice);
 }
 
-function makeRandom() {
-    const min = 0;
-    const max = loadMachineChoice;
-    console.log(max)
-    const RandVal = Math.floor(Math.random()*(max-min+1)) + min;
-    return RandVal;
-}
-
-function init(){
-    rangeBar.addEventListener("input", function(){
-        range.innerText = `Generate a Number between 0 and ${rangeBar.value}`;
-        saveMachineChoice(rangeBar.value);
-
-    }, false);
+function init() {
+  button.addEventListener("click", handleFormSubmit);
+  range.innerText = `Generate a Number between 0 and ${rangeBar.value}`;
+  rangeBar.addEventListener(
+    "input",
+    function() {
+      range.innerText = `Generate a Number between 0 and ${rangeBar.value}`;
+    },
+    false
+  );
 }
 
 init();
